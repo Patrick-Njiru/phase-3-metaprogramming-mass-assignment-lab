7a992e78d49b6e5fecde46ce0c54963bfcd4713d
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-27 15:58:32 +0300

    Completed assignment.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  # your code here
+
+  def initialize attributes
+    attributes.each_key do |key|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", attributes[key])
+    end
+  end
 end
